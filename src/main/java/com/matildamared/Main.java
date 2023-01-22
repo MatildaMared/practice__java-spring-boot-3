d0de51cf4416f594bedf681306d3c24beb2e4328
Matilda Mared <matildamared@live.se>
2023-01-22 09:21:40 +0100

    update configuration files with database related stuff

--- src/main/java/com/matildamared/Main.java
+++ src/main/java/com/matildamared/Main.java
@@ -2,10 +2,31 @@ package com.matildamared;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
+    }
+
+    @GetMapping("/")
+    public GreetResponse greet() {
+        return new GreetResponse(
+                "Hello, friend!",
+                List.of("JavaScript", "TypeScript", "Node.js", "CSS"),
+                new Person("Matilda", 32)
+        );
+    }
+
+    record Person(String name, int age) {
+    }
+
+    record GreetResponse(String greet, List<String> favoriteProgrammingLanguages, Person person) {
+
     }
 }
